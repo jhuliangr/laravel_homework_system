e21bbd19d55b516c551f72143ac67d355d818f3f
jhuliangr <jhpiano9731@gmail.com>
2025-11-13 16:44:34 +0100

    ADD evaluation migration and model

---
+++ app/Models/Evaluation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+    /** @use HasFactory<\Database\Factories\CalificationFactory> */
+    use HasFactory;
+
+    public function homework()
+    {
+        return $this->belongsTo(Homework::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+}
