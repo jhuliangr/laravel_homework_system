330c0b94b8def956f53dcead3463d44a1c84fc55
jhuliangr <jhpiano9731@gmail.com>
2025-11-17 19:22:01 +0100

    ADD support for enrolling users into courses

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -1,9 +1,9 @@
 <x-site-layout>
     <div class="bg-gray-200 w-full flex items-center justify-between rounded-xl p-3 mb-5">
-        <a href="{{ route('course.index') }}" class="bg-teal-300 p-2 rounded-xl">
-            Back
-        </a>
-        @include('course.create-course-form')
+        @include('components.back-button')
+        @if ($edit)
+            @include('course.create-course-form')
+        @endif
     </div>
     <div class="px-10">
         <div class="bg-gray-100 border rounded-xl p-3">
@@ -14,12 +14,27 @@
         </div>
     </div>
     <div class="w-full h-52 flex items-center justify-center">
-        <form action="{{ route('course.destroy', $course->id) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="bg-red-500 py-2 px-10 rounded-xl text-white">
-                Delete
-            </button>
-        </form>
+        @if ($edit)
+            @if ($user->teacher)
+                <form action="{{ route('course.destroy', $course->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="bg-red-500 py-2 px-10 rounded-xl text-white">
+                        Delete
+                    </button>
+                </form>
+            @endif
+        @else
+            @if (!$user->courses->pluck('id')->contains($course->id))
+                <form action="{{ route('course.enroll_in', $course->id) }}" method="POST">
+                    @csrf
+                    <button type="submit" class="bg-teal-500 py-2 px-10 rounded-xl text-white">
+                        Enroll in
+                    </button>
+                </form>
+            @else
+                subir tarea :D
+            @endif
+        @endif
     </div>
 </x-site-layout>
