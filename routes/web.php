954eb9f687d84dd50bec83df4ab1149e0f3a9f2d
jhuliangr <jhpiano9731@gmail.com>
2025-11-18 19:40:11 +0100

    ADD is_teacher middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
 
-Route::get('/homework', [\App\Http\Controllers\HomeworkController::class, 'index']) -> name('homework.index');
-Route::get('/homework/{id}', [\App\Http\Controllers\HomeworkController::class, 'show']) -> name('homework.show');
-
-
-
 Route::get('/dashboard', function () {
     $user = Auth::user();
     $teacher = $user->teacher;
     return view('userzone.dashboard', compact('teacher', 'user'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'is_teacher'])->group(function () {
     Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
-
+    
     Route::resource('course', \App\Http\Controllers\CourseController::class);
     Route::get('/my_courses', [App\Http\Controllers\CourseController::class, 'user_courses'])->name('courses.my_courses');
     Route::get('/pick_courses', [App\Http\Controllers\CourseController::class, 'pick'])->name('course.pick');
@@ -27,4 +22,4 @@ Route::middleware('auth')->group(function () {
     
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
