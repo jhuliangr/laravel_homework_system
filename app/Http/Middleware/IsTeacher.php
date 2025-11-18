29ed305078ba66185df3cb882a6c767a5255f667
jhuliangr <jhpiano9731@gmail.com>
2025-11-18 19:32:12 +0100

    ADD isTeacher middleware

---
+++ app/Http/Middleware/IsTeacher.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsTeacher
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->merge([
+            'is_teacher' => auth()->check() ? auth()->user()->isTeacher() : false
+        ]);
+        return $next($request);
+    }
+}
