9940435743f16b3afae9704f37f71b022bf38bc0
jhuliangr <jhpiano9731@gmail.com>
2025-11-19 13:32:36 +0100

    FIX restyled course-row component

--- resources/views/components/course-row.blade.php
+++ resources/views/components/course-row.blade.php
@@ -1,5 +1,5 @@
 <a href="/course/{{ $course->id }}"
-    class="grid grid-cols-2 bg-gradient-to-r from-teal-200 to-teal-600 my-1 px-5 py-3 rounded-xl cursor-pointer hover:bg-teal-100 transition-colors duration-300 ease-in">
+    class="grid grid-cols-2 bg-gradient-to-r from-teal-200 to-teal-600 hover:from-teal-500 hover:to-teal-100 my-1 px-5 py-3 rounded-xl cursor-pointer shadow-md hover:shadow-sm">
     <div>
         {{ $course->module_name }}
     </div>
