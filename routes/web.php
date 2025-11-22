53dfbe597994f4bd6c5af114d98bd5c9b167179c
jhuliangr <jhpiano9731@gmail.com>
2025-11-22 16:33:09 +0100

    ADD routes for evaluations

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,18 @@ Route::middleware(['auth', 'is_teacher'])->group(function () {
     Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+
     Route::resource('course', \App\Http\Controllers\CourseController::class);
     Route::get('/my_courses', [App\Http\Controllers\CourseController::class, 'user_courses'])->name('courses.my_courses');
     Route::get('/pick_courses', [App\Http\Controllers\CourseController::class, 'pick'])->name('course.pick');
     Route::post('/enroll_in/{course_id}', [App\Http\Controllers\CourseController::class, 'enroll_in'])->name('course.enroll_in');
-    
+
     Route::get('/homework/{courseId}', [\App\Http\Controllers\HomeworkController::class, 'index'])->name('homework.index');
     Route::get('/homework/show/{id}', [\App\Http\Controllers\HomeworkController::class, 'show'])->name('homework.show');
     Route::post('/homework/{id}', [\App\Http\Controllers\HomeworkController::class, 'store'])->name('homework.store');
+    Route::post('/homework/evaluate/{id}', [\App\Http\Controllers\HomeworkController::class, 'evaluate'])->name('homework.evaluate');
+    Route::post('/homework/reevaluate/{id}', [\App\Http\Controllers\HomeworkController::class, 'reEvaluate'])->name('homework.reEvaluate');
+
 });
 
 require __DIR__ . '/auth.php';
