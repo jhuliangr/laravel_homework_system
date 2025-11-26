c4f6ca10d2410604943b6507a304724217f4b83b
jhuliangr <jhpiano9731@gmail.com>
2025-11-26 15:05:57 +0100

    FIX back button triggering forms behaviour removed

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,7 +1,7 @@
 @props(['route' => 'default', 'param1' => ''])
 
 <a href="{{ $route == 'default' ? url()->previous() : route($route, $param1) }}">
-    <x-breeze.primary-button>
+    <x-breeze.primary-button type="button">
         Back
     </x-breeze.primary-button>
 </a>
