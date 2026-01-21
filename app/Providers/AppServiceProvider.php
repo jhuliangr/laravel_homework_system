a087e69477243953a3ca17ce2d4f30486457b0e4
jhuliangr <jhpiano9731@gmail.com>
2026-01-21 13:51:09 +0100

    Add backups with google drive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Google\Exception;
 use Illuminate\Support\ServiceProvider;
+use Storage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->loadGoogleStorageDriver('google');
+        $this->loadGoogleStorageDriver('google_admin');
+    }
+    private function loadGoogleStorageDriver(string $driverName = 'google')
+    {
+        try {
+            Storage::extend($driverName, function ($app, $config) {
+                $options = [];
+
+                $client = new \Google\Client();
+                $client->setClientId($config['clientId']);
+                $client->setClientSecret($config['clientSecret']);
+                $client->refreshToken($config['refreshToken']);
+
+                $service = new \Google\Service\Drive($client);
+                $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service,  '/', $options);
+                $driver = new \League\Flysystem\Filesystem($adapter);
+
+                return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
+            });
+        } catch (\Exception $e) {
+            logger()->error($e->getMessage());
+        }
     }
 }
