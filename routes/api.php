a630dfc9fd8a3651235cd97967fd776090572504
jhuliangr <jhpiano9731@gmail.com>
2026-01-30 08:25:43 +0100

    FIX external api for charts data

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\HomeworkPublicApiController;
-use App\Http\Controllers\ChartsDataController;
+use \App\Http\Controllers\Api\ChartsDataController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('mollie/webhook', \App\Http\Controllers\Api\MollieWebhookController::class)->name('api.mollie.webhook');
 
-Route::middleware('api')->prefix('api')->group(function () {
+Route::get('charts-info', [ChartsDataController::class, 'index']);
 
-  Route::middleware('auth:sanctum')->group(function () {
-    Route::get('charts-info', [ChartsDataController::class, 'index']);
-    Route::get('/courses/{courseId}/homeworks', [HomeworkPublicApiController::class, 'index']);
-    Route::post('/courses/{courseId}/homeworks', [HomeworkPublicApiController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
+  Route::get('/courses/{courseId}/homeworks', [HomeworkPublicApiController::class, 'index']);
+  Route::post('/courses/{courseId}/homeworks', [HomeworkPublicApiController::class, 'store']);
 
-    Route::get('/homeworks/{id}', [HomeworkPublicApiController::class, 'show']);
-    Route::get('/homeworks/search', [HomeworkPublicApiController::class, 'search']);
+  Route::get('/homeworks/{id}', [HomeworkPublicApiController::class, 'show']);
+  Route::get('/homeworks/search', [HomeworkPublicApiController::class, 'search']);
 
-    Route::post('/homeworks/{id}/evaluate', [HomeworkPublicApiController::class, 'evaluate']);
-    Route::put('/homeworks/{id}/evaluate', [HomeworkPublicApiController::class, 'reEvaluate']);
-  });
+  Route::post('/homeworks/{id}/evaluate', [HomeworkPublicApiController::class, 'evaluate']);
+  Route::put('/homeworks/{id}/evaluate', [HomeworkPublicApiController::class, 'reEvaluate']);
 });
